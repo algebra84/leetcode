e3f61263241c809df2aa4868688906a0c30f086b
algebra84 <crazylht@126.com>
2017-05-22 17:26:29 +0800

    Divide and Conquer

---
+++ src/divideandconquer/findMedianSortedArrays.cpp
@@ -0,0 +1,23 @@
+/*4. Median of Two Sorted Arrays
+  There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+  Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+  Example 1:
+  nums1 = [1, 3]
+  nums2 = [2]
+
+  The median is 2.0
+  Example 2:
+  nums1 = [1, 2]
+  nums2 = [3, 4]
+
+  The median is (2 + 3)/2 = 2.5*/
+
+#include<vector>
+using namespace std;
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+  double res = 0.0;
+  int num1 = nums1.size();
+  int num2 = nums2.size();
+}
