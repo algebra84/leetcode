b7540bf3748991f0eca220f51de51f03477bea80
algebra84 <crazylht@126.com>
2017-05-25 14:40:31 +0800

    DP

--- src/divideandconquer/findRotateSteps.cpp
+++ src/divideandconquer/findRotateSteps.cpp
@@ -1,23 +1,37 @@
 /*514. Freedom Trail
   https://leetcode.com/problems/freedom-trail/#/description
  */
-
+#include<algorithm>
 #include<string>
+#include<map>
+#include<vector>
+#include<iostream>
 using namespace std;
 int findRotateSteps(string ring, string key){
-  int size_k = key.size();
-  if(size_k == 1){
-    int res1,res2;
-    for(int i = 0; i != ring.size();i++)
-      if(ring[i] == key[0]){
-        res1 = i;
-        break;
+  map<char, vector<int> > mp;
+  int size = ring.size();
+  int ksize = key.size();
+  for(int i = 0; i != size; i++)
+    mp[ring[i]].push_back(i);
+  vector<vector<int> > result(ksize+1,vector<int> (size,10000));
+  for(int i = 0; i != size; i++)
+    result[0][i] = 0;
+  vector<int> tmp(1,0);
+  for(int i = 1; i <= ksize; i++){
+    for(auto it:mp[key[i-1]])
+      for(int j = 0; j != tmp.size(); j++){
+        int res = min((it+size-tmp[j])%size,(tmp[j]+size-it)%size);
+        result[i][it] = min(result[i][it],res + result[i-1][tmp[j]]);
       }
-    for(int i = ring.size()-1; i != -1; i--)
-      if(ring[i] == key[i]){
-        res2 = ring.size()-1-i;
-        break;
-      }
-    return res1>res2?(res2+1):(res1+1);
+    tmp = mp[key[i-1]];
   }
+  int res = 10000;
+  for(int i = 0; i != mp[key[ksize-1]].size(); i++)
+    res = min(res,result[ksize][mp[key[ksize-1]][i]]);
+  return res+ksize;
+}
+
+int main()
+{
+  cout<<findRotateSteps("godding","godding")<<endl;
 }
