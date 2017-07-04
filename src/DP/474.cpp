90aef9a7624933f7df1a3c3f1c52ac9b0f41e7e1
algebra84 <crazylht@126.com>
2017-07-04 14:05:34 +0800

    DP

--- src/DP/474.cpp
+++ src/DP/474.cpp
@@ -2,27 +2,39 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<utility>
 #include<algorithm>
+#include<unordered_map>
 
 using namespace std;
-
-int find_1(vector<pair<int,int> > &cube,int m, int n,
-           vector<int> &visited){
+int find_1(vector<vector<int> > &cube,int m, int n,int index,
+           unordered_map<string,int>& mmap){
+  if(index == cube.size())
+    return 0;
+  if(m == 0 && n == 0)
+    return 0;
+  string key = to_string(m)+" "+to_string(n)+" "+to_string(index);
+  if(mmap.count(key))
+    return mmap[key];
   int res = 0;
-  for(int i = 0; i != visited.size();i++){
-    if(visited[i])
-      continue;
-    visited[i] = 1;
-    if(m-cube[i].first >= 0 && n-cube[i].second>=0)
-      res = max(res,1+find_1(cube,m-cube[i].first,n-cube[i].second,visited));
-    visited[i] = 0;
+  int max_m = (m+n),max_n = (m+n);
+  if(cube[index][2]!= 0)
+    max_m = m/cube[index][2];
+  if(cube[index][3]!=0)
+    max_n = n/cube[index][3];
+  for(int i = index; i != cube.size();i++){
+    if(m-cube[i][0] >= 0 && n-cube[i][1]>=0)
+      res = max(res,1+find_1(cube,m-cube[i][0],n-cube[i][1],
+                             i+1,mmap ));
+    if(res == min(max_n,max_n))
+      break;
   }
+  mmap[key]=res;
   return res;
 }
 int findMaxForm(vector<string>& strs, int m, int n){
   int size = strs.size();
-  vector<pair<int,int> > cube;
+  vector<vector<int> > cube(size,vector<int>(4,0));
+  int min_m = 1000,min_n = 1000;
   for(int i = 0; i != strs.size(); i++){
     int m0=0,n1=0;
     for(int j = 0; j != strs[i].size();j++)
@@ -30,15 +42,21 @@ int findMaxForm(vector<string>& strs, int m, int n){
         m0++;
       else
         n1++;
-    cube.push_back(make_pair(m0,n1));
+    cube[i][0] = m0;
+    cube[i][1] = n1;
   }
-
-  vector<int> visited(size,0);
-  return find_1(cube,m,n,visited);
+  for(int i = cube.size()-1; i != -1; i--){
+    min_m = min(min_m,cube[i][0]);
+    min_n = min(min_n,cube[i][1]);
+    cube[i][2] = min_m;
+    cube[i][3] = min_n;
+  }
+  unordered_map<string,int> mmap;
+  return find_1(cube,m,n,0,mmap);
 }
 int main()
 {
-  string s[]={"0","1101","01","00111","1","10010","0","0","00","1","11","0011"};
-  vector<string> strs(s,s+12);
-  cout<<findMaxForm(strs,63,36)<<endl;
+  string s[]={"0","0","0","0","0","1","1","0","1","1","1","0","1","0","1","1","0","0","1","0","1","1","0","1","1","1","1","1","0","1","1","1","1","1","1","0","1","1","0","0","0","0","1","1","0","1"};
+  vector<string> strs(s,s+46);
+  cout<<findMaxForm(strs,52,12)<<endl;
 }
