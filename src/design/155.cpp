ca4514eae3072ae5323676efa8969786ccdf79c0
algebra84 <crazylht@126.com>
2017-07-18 17:33:46 +0800

    design

---
+++ src/design/155.cpp
@@ -0,0 +1,25 @@
+/* 155. Min Stack */
+// all in O(1) time
+class MinStack {
+public:
+  /** initialize your data structure here. */
+  MinStack() {
+
+  }
+
+  void push(int x) {
+
+  }
+
+  void pop() {
+
+  }
+
+  int top() {
+
+  }
+
+  int getMin() {
+
+  }
+};
