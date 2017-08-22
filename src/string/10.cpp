130169ef8f0388cbb1a0e1ea31c527004dc9c53d
algebra84 <crazylht@126.com>
2017-08-22 15:21:34 +0800

    string

--- src/string/10.cpp
+++ src/string/10.cpp
@@ -1,59 +1,72 @@
 /* 10. Regular Expression Matching */
 #include<string>
 #include<iostream>
+#include<vector>
 using namespace std;
 
-bool isMatch(string s, string p) {
-  if(s.size() == 0)
-    return p.size()==0;
-  int i = 0,j = 0;
-  while(i != s.size()){
-    if(p[j] == '.'){
-      i++;
-      j++;
-      continue;
-    }
-    if(p[j] == '*'){
-      int tmp = i-1;
-      while(s[tmp] == s[i-1] && tmp != s.size())
-        tmp++;
-      while(p[j] == '*' && j != p.size())
-        j++;
-      if(j == p.size())
-        return tmp==s.size();
-      if(p[j] == '.'){
-        for(int idx = i; idx != tmp+1; idx++){
-          if(isMatch(s.substr(idx,s.size()-idx),p.substr(j,p.size()-j)))
-            return true;
-        }
-        return false;
-      }
-      if(p[j] == s[i-1]){
-        for(int idx = i; idx != tmp; idx++){
-          if(isMatch(s.substr(idx,s.size()-idx),p.substr(j,p.size()-j)))
-            return true;
-        }
-        return false;
-      }
-      if(p[j] != s[i-1]){
-        if(tmp == s.size())
-          return false;
-        if(s[tmp] != p[j])
-          return false;
-        return isMatch(s.substr(tmp,s.size()-tmp),p.substr(j,p.size()-j));
-      }
-    }
-    if(s[i]==p[j]){
-      i++;
-      j++;
-    }
+bool isMatch_1(string s, int id1,
+               string p, int id2,
+               vector<vector<int> > &mmap)
+{
+  if(id2 == p.size()){
+    if(id1 == s.size())
+      return true;
     else
       return false;
   }
-  return true;
+  if(id2 == p.size()-1){
+    if(id1 != s.size()-1)
+      return false;
+    if(p[id2] == '.')
+      return true;
+    return p[id2] == s[id1];
+  }
+  if(id1 == s.size()){
+    if(p[id2+1] != '*')
+      return false;
+    int tmp = id2+1;
+    while(p[tmp]=='*'){
+      tmp++;
+    }
+    if(tmp == p.size())
+      return true;
+    return false;
+  }
+  if(p[id2+1] != '*'){
+    if(p[id2] = '.')
+      return isMatch_1(s,id1+1,p,id2+1,mmap);
+    if(p[id2] == s[id1])
+      return isMatch_1(s,id1+1,p,id2+1,mmap);
+    return false;
+  }
+  int tmp = id2+1;
+  while(p[tmp] == '*')
+    tmp++;
+  if(p[id2] != '.'){
+    int tmp1 = id1;
+    if(s[id1] != p[id2])
+      return isMatch_1(s,id1,p,tmp,mmap);
+    while(s[tmp1] == s[id1])
+      tmp1++;
+    for(int i = id1; i != tmp1+1; i++){
+      if(isMatch_1(s,i,p,tmp,mmap))
+        return true;
+    }
+  }
+  for(int i = id1; i != s.size()+1; i++)
+    if(isMatch_1(s,i,p,tmp,mmap))
+      return true;
+  return false;
+}
+
+bool isMatch(string s, string p) {
+  vector<vector<int> > mmap(s.size(),vector<int> (p.size(), -1));
+  return isMatch_1(s,0,p,0,mmap);
 }
 
 int main()
 {
-  cout<<isMatch("ab",".*")<<endl;
+  cout<<isMatch("ab",".")<<endl;
+  cout<<isMatch("aab","c*a*b")<<endl;
+  return 1;
 }
